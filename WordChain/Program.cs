cd42c45308643b4620133f355cddedfd7b2cfd5e
NanonaN <zhouyufei1998@vip.qq.com>
2019-03-06 18:22:01 +0800

    build basic structure

--- WordChain/Program.cs
+++ WordChain/Program.cs
@@ -1,15 +1,221 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WordChain
 {
     class Program
     {
+        string inputFilePath = "";
+        char head = '\0';
+        char tail = '\0';
+        bool enableLoop = false;
+        bool wordMode = false;
+        bool charMode = false;
+        string outputFilePath = "solution.txt";
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.GenerateChain();
+        }
+        void GenerateChain(bool byCommandLine = false, string[] args = null)
+        {
+            if (byCommandLine)
+            {
+                if (args == null)
+                {
+                    ExitWithCause("command line has got null arguments");
+                }
+                ParseCommandLineArguments(args);
+            }
+            string content = ReadContentFromFile();
+            List<string> words = DivideWord(content);
+            List<string> chain = new List<string>();
+            if (wordMode)
+            {
+                chain = FindLongestChainByWord(words);
+            }
+            if (charMode)
+            {
+                chain = FindLongestChainByChar(words);
+            }
+            OutputChain(chain);
+        }
+        Program()
+        {
+
+        }
+        Program(string inputFilePath, int mode = 0, char head = '\0', char tail = '\0', bool enableLoop = false, string outputFilePath = @"solution.txt")
+        {
+            if (mode == 0)
+            {
+                wordMode = true;
+            }
+            else if (mode == 1)
+            {
+                charMode = true;
+            }
+            else
+            {
+                ExitWithCause("program mode not provided");
+            }
+            this.inputFilePath = inputFilePath;
+            this.head = head;
+            this.tail = tail;
+            this.enableLoop = false;
+            this.outputFilePath = outputFilePath;
+        }
+        void ParseCommandLineArguments(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Equals("-w"))
+                {
+                    if (args.Length <= i + 1)
+                    {
+                        ExitWithCause("-w argument requires a file absolute path");
+                    }
+                    else
+                    {
+                        if (wordMode == true)
+                        {
+                            ExitWithCause("-w argument cannot be used twice");
+                        }
+                        if (charMode == true)
+                        {
+                            ExitWithCause("-w argument cannot be used together with -c argument");
+                        }
+                        inputFilePath = args[++i];
+                        wordMode = true;
+                        continue;
+                    }
+                }
+                if (args[i].Equals("-c"))
+                {
+                    if (args.Length <= i + 1)
+                    {
+                        ExitWithCause("-c argument requires a file absolute path");
+                    }
+                    else
+                    {
+                        if (charMode == true)
+                        {
+                            ExitWithCause("-c argument cannot be used twice");
+                        }
+                        if (wordMode == true)
+                        {
+                            ExitWithCause("-c argument cannot be used together with -w argument");
+                        }
+                        inputFilePath = args[++i];
+                        charMode = true;
+                        continue;
+                    }
+                }
+                if (args[i].Equals("-h"))
+                {
+                    if (args.Length <= i + 1)
+                    {
+                        ExitWithCause("-h argument requires a start letter");
+                    }
+                    else
+                    {
+                        if (head != '\0')
+                        {
+                            ExitWithCause("-h argument cannot be used twice");
+                        }
+                        string headString = args[++i];
+                        if (headString.Length != 1 || !char.IsLetter(headString[0]))
+                        {
+                            ExitWithCause("-h argument requires a start letter");
+                        }
+                        head = headString[0];
+                        continue;
+                    }
+                }
+                if (args[i].Equals("-t"))
+                {
+                    if (args.Length <= i + 1)
+                    {
+                        ExitWithCause("-t argument requires an end letter");
+                    }
+                    else
+                    {
+                        if (tail != '\0')
+                        {
+                            ExitWithCause("-t argument cannot be used twice");
+                        }
+                        string tailString = args[++i];
+                        if (tailString.Length != 1 || !char.IsLetter(tailString[0]))
+                        {
+                            ExitWithCause("-t argument requires an end letter");
+                        }
+                        tail = tailString[0];
+                        continue;
+                    }
+                }
+                if (args[i].Equals("-r"))
+                {
+                    if (enableLoop)
+                    {
+                        ExitWithCause("-r argument cannot be used twice");
+                    }
+                    enableLoop = true;
+                    continue;
+                }
+                ExitWithCause("invalid argument" + args[i]);
+            }
+        }
+        private void ExitWithCause(string message)
+        {
+            Console.WriteLine(message);
+            System.Environment.Exit(1);
+        }
+        private string ReadContentFromFile()
+        {
+            if (!File.Exists(inputFilePath))
+            {
+                ExitWithCause("file " + inputFilePath + " not found");
+            }
+            string content = "";
+            try
+            {
+                content = File.ReadAllText(inputFilePath);
+            }
+            catch (Exception)
+            {
+                ExitWithCause("unable to read from file " + inputFilePath);
+            }
+            return content;
+        }
+        private List<string> DivideWord(string content)
+        {
+            string[] words = System.Text.RegularExpressions.Regex.Split(content, @"[^a-zA-Z]+");
+            List<string> wordList = new List<string>();
+            foreach (string word in words)
+            {
+                wordList.Add(word.ToLower());
+            }
+            return wordList;
+        }
+        private List<string> FindLongestChainByWord(List<string> words)
+        {
+
+        }
+        private List<string> FindLongestChainByChar(List<string> words)
+        {
+
+        }
+        private void OutputChain(List<string> chain)
+        {
+            string[] result = chain.ToArray();
+            try
+            {
+                File.WriteAllLines(outputFilePath, chain);
+            }
+            catch (Exception e)
+            {
+                ExitWithCause("Error Occured When Writing Result To File");
+            }
         }
     }
 }
